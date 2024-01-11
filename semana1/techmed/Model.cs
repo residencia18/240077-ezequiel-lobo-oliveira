d92ec1dcf94c554d71287661153376ddfd11a94e
EzekLobo <eloliveira.cic@uesc.br>
2024-01-11 15:13:28 -0300

    Relacionamentos

--- semana1/techmed/Model.cs
+++ semana1/techmed/Model.cs
@@ -2,12 +2,16 @@ using Microsoft.EntityFrameworkCore;
 
 namespace techmed;
 
-public class TechmedContext : DbContext{
-    public DbSet<Medico> Medicos {get; set;}
-    public DbSet<Paciente> Pacientes{get; set;}
-    protected override void OnConfiguring (DbContextOptionsBuilder optionsBuilder){
+public class TechmedContext : DbContext
+{
+    public DbSet<Medico> Medicos { get; set; }
+    public DbSet<Paciente> Pacientes { get; set; }
+    public DbSet<Atendimentos> Atendimentos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
         base.OnConfiguring(optionsBuilder);
-        var connectionString="server=localhost;Database=techmed;user=root;password=12345";
+        var connectionString = "server=localhost;Database=techmed;user=root;password=12345";
         var serverVersion = ServerVersion.AutoDetect(connectionString);
         optionsBuilder.UseMySql(connectionString, serverVersion);
     }
@@ -15,26 +19,50 @@ public class TechmedContext : DbContext{
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Medico>().ToTable("Medicos").HasKey(m=>m.Id);
-        modelBuilder.Entity<Paciente>().ToTable("Pacientes").HasKey(m=>m.Id);
+        modelBuilder.Entity<Medico>().ToTable("Medicos").HasKey(m => m.Id);
+        modelBuilder.Entity<Paciente>().ToTable("Pacientes").HasKey(m => m.Id);
+        modelBuilder.Entity<Atendimentos>().ToTable("Atendimentos").HasKey(a => a.Id);
+
+        modelBuilder.Entity<Atendimentos>()
+            .HasOne(a => a.Medico)
+            .WithMany(m => m.Atendimentos)
+            .HasForeignKey(a => a.MedicoId);
+
+        modelBuilder.Entity<Atendimentos>()
+            .HasOne(a => a.Paciente)
+            .WithMany(p => p.Atendimentos)
+            .HasForeignKey(a => a.PacienteId);
     }
 }
-public abstract class Pessoa{
-    public int Id { get; set;}
-    public string Nome {get;set;}
-    public string CPF {get;set;}
 
-}
-public class Medico : Pessoa{
-  public string CRM {get; set;}
-  public string Especialidade {get; set;}
-  public float Salario {get; set;}
+public abstract class Pessoa
+{
+    public int Id { get; set; }
+    public required string Nome { get; set; }
+    public required string CPF { get; set; }
 }
 
-public class Paciente:Pessoa{
+public class Medico : Pessoa
+{
+    public required string CRM { get; set; }
+    public required string Especialidade { get; set; }
+    public float Salario { get; set; }
+    public List<Atendimentos> Atendimentos { get; set; }
+}
 
-    public string Endereco {get; set;}
-    public string Telefone {get; set;}
+public class Paciente : Pessoa
+{
+    public required string Endereco { get; set; }
+    public required string Telefone { get; set; }
+    public List<Atendimentos> Atendimentos { get; set; }
+}
 
-
+public class Atendimentos
+{
+    public int Id { get; set; }
+    public DateTime DataHora { get; set; }
+    public int MedicoId { get; set; }
+    public Medico Medico { get; set; }
+    public int PacienteId { get; set; }
+    public Paciente Paciente { get; set; }
 }
--- semana1/techmed/Program.cs
+++ semana1/techmed/Program.cs
@@ -1,5 +1,16 @@
-﻿using techmed;
+﻿using System.ComponentModel.DataAnnotations;
+using techmed;
 var context =new TechmedContext();
+
+    // Cria um novo paciente
+    context.Pacientes.Add(new Paciente{
+        Nome = "Nome do Paciente",
+        CPF = "123.456.789-00",
+        Endereco = "Endereço do Paciente",
+        Telefone = "1111-2222",
+    });
+    context.SaveChanges();
+
 // context.Medicos.Add(new Medico{
 //     Nome="g",
 //     CPF ="2",
@@ -9,4 +20,8 @@ var context =new TechmedContext();
 // });
 //  context.SaveChanges();
 
+
+Console.WriteLine("Medicos:");
 context.Medicos.ToList().ForEach(m=> Console.WriteLine($"{m.Id}-{m.CRM}-{m.Nome}-{m.CPF}-{m.Salario}"));
+Console.WriteLine("Pacientes:");
+context.Pacientes.ToList().ForEach(m=> Console.WriteLine($"{m.Id}-{m.CPF}-{m.Nome}-{m.Endereco}-{m.Telefone}"));
