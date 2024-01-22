b837193d9c59111dff80b9a79d00f98981e52589
EzekLobo <eloliveira.cic@uesc.br>
2024-01-22 09:53:42 -0300

    DbContext

---
+++ semana2/techmed.Domain/Entities/Exame.cs
@@ -0,0 +1,10 @@
+namespace techmed.Domain.Entities;
+
+public class Exame
+{
+     public int ExameId {get; set;}
+    public string Local {get; set;}
+    public DateTime DataHora {get; set;}
+    public ICollection<Atendimento>? Atendimentos {get; set;}
+
+}
