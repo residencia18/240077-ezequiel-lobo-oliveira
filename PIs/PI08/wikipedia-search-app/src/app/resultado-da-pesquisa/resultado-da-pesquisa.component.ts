66ef5fb2b057c1995f10d5f8390cd1542aba9d5e
EzekLobo <eloliveira.cic@uesc.br>
2024-01-25 16:41:58 -0300

    Otimização

--- PIs/PI08/wikipedia-search-app/src/app/resultado-da-pesquisa/resultado-da-pesquisa.component.ts
+++ PIs/PI08/wikipedia-search-app/src/app/resultado-da-pesquisa/resultado-da-pesquisa.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SecurityContext } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-resultado-da-pesquisa',
@@ -6,12 +7,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./resultado-da-pesquisa.component.css'],
 })
 export class ResultadoDaPesquisaComponent {
-  @Input() resultado: any[] = [];
-
-  constructor() {}
+  @Input() resultado: any[] = []; 
+  constructor(private sanitizer: DomSanitizer) {}
 
   getArticleLink(article: any): string {
-    // Construa o link para a página completa do artigo na Wikipédia
     return `https://en.wikipedia.org/wiki/${encodeURIComponent(article.title)}`;
+  }
+
+  sanitizeHtml(html: string | null): SafeHtml | null {
+    return html ? this.sanitizer.sanitize(SecurityContext.HTML, html) : null;
   }
 }
