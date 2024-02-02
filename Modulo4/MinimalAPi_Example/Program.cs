387509057b3afa46af531eb43e248ee505ccd00d
EzekLobo <eloliveira.cic@uesc.br>
2024-02-02 15:35:33 -0300

    Add middleware classes and project files

---
+++ Modulo4/MinimalAPi_Example/Program.cs
@@ -0,0 +1,18 @@
+using Modulo4.LinhaDeMontagem;
+using static Modulo4.LinhaDeMontagem.LinhaDeMontagemDescricao;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<LinhaDeMontagemDescricao>();
+
+var app = builder.Build();
+//app.UseHttpsRedirection();
+
+app.UseMiddleware<CustomHeaderMiddleware>();
+app.UseMiddleware<AddChassiMiddleware>();
+app.UseMiddleware<AddMotorMiddleware>();
+app.UseMiddleware<AddPortasMiddleware>();
+app.UseMiddleware<AddPinturaMiddleware>();
+app.UseMiddleware<AddInternoMiddleware>();
+
+app.Run();
