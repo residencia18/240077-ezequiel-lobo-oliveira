2a56186d7203ea80b580801ce8710df788cea103
EzekLobo <eloliveira.cic@uesc.br>
2024-02-06 02:44:17 -0300

    Add database configuration and connection string

--- TechMed.WebAPI/Program.cs
+++ TechMed.WebAPI/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+using TechMed.Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddDbContext<TechMedDbContext>(options => {
+    var connectionString = builder.Configuration.GetConnectionString("TechMedDb");
+    var serverVersion = ServerVersion.AutoDetect(connectionString);
+    options.UseMySql(connectionString, serverVersion);
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
