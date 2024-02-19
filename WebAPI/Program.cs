037f1a821f8b3abb27058d8c57572e9c0bf0f600
EzekLobo <eloliveira.cic@uesc.br>
2024-02-19 16:22:30 -0300

    Add new WebAPI project and middlewares

---
+++ WebAPI/Program.cs
@@ -0,0 +1,18 @@
+using Middleware.Extensions;
+using Modulo4.LinhaDeMontagem;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<LinhaDeMontagemDescricao>();
+
+var app = builder.Build();
+
+
+app.UseAddChassiMiddleware();
+app.UseAddMotorMiddleware();
+app.UseMiddleware<AddPortasMiddleware>();
+app.UseMiddleware<AddPinturaMiddleware>();
+app.UseMiddleware<AddInternoMiddleware>();
+
+
+app.Run();
