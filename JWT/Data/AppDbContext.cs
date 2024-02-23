f4cd8272253dcced3a14b9ad0e1d3e02a41ca439
EzekLobo <eloliveira.cic@uesc.br>
2024-02-22 23:13:53 -0300

    Remove unused package reference and update database connection string

--- JWT/Data/AppDbContext.cs
+++ JWT/Data/AppDbContext.cs
@@ -1,23 +1,32 @@
-namespace JWT.Model;
+using JWT.Model;
 using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext : DbContext 
-{
-   public DbSet<UsuarioModel> Usuarios { get; set; }
 
-   protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+namespace WT.Model;
+
+public class AppDbContext : DbContext
+{
+    
+    public DbSet<UsuarioModel> Usuarios { get; set; }
+    
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         base.OnConfiguring(optionsBuilder);
-        var connectionString = "server=localhost;user=root;password=12345;database=jws";
+        var connectionString = "server=localhost;user=root;password=12345;database=jwt";
         var serverVersion = ServerVersion.AutoDetect(connectionString);
-        
+
         optionsBuilder.UseMySql(connectionString, serverVersion);
-        
     }
 
-   protected override void OnModelCreating(ModelBuilder modelBuilder)
-   {
-      base.OnModelCreating(modelBuilder);
-      modelBuilder.Entity<UsuarioModel>().ToTable("Usuarios").HasKey(a => a.Id);
-   }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        
+        modelBuilder.Entity<UsuarioModel>().ToTable("Usuarios").HasKey(s => s.Id);
+        
+
+      
+    }
 }
--- JWT/Program.cs
+++ JWT/Program.cs
@@ -1,19 +1,13 @@
-using Microsoft.EntityFrameworkCore;
-using JWT.Model;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<AppDbContext>();
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -23,15 +17,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
-app.UseRouting(); // Adicione o middleware de roteamento
-
-// Adicione o middleware do Entity Framework Core para aplicar migrações pendentes
-using (var scope = app.Services.CreateScope())
-{
-    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    context.Database.Migrate(); // Aplica migrações pendentes, se houver
-}
 
 app.MapControllers();
 
