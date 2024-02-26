c8be0b10cad4ada60bdaf5f47302613a91c4ec3b
EzekLobo <eloliveira.cic@uesc.br>
2024-02-26 20:07:30 -0300

    Update Id properties to more descriptive names

--- Modulo5/MvcMovie/Models/Movie.cs
+++ Modulo5/MvcMovie/Models/Movie.cs
@@ -4,7 +4,7 @@ namespace Mvc.Models;
 
 public class Movie
 {
-    public int Id { get; set; }
+    public int MovieId { get; set; }
     public string? Title { get; set; }
 
     [DataType(DataType.Date)]
--- Modulo5/MvcMovie/Models/User.cs
+++ Modulo5/MvcMovie/Models/User.cs
@@ -1,7 +1,7 @@
 namespace Mvc.Models;
 public class User
 {
-    public int Id { get; set; }
+    public int UserId { get; set; }
     public required string Name { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
