5c298a7b13e56a4f9b5519be67cea43e05576775
EzekLobo <eloliveira.cic@uesc.br>
2024-03-01 13:59:19 -0300

    Add Angular Material modules to app module

--- PIs/PI11/FormularioUsuarios/src/app/app.module.ts
+++ PIs/PI11/FormularioUsuarios/src/app/app.module.ts
@@ -1,12 +1,14 @@
-// app.module.ts
 
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
@@ -14,16 +16,19 @@ import { FormUsuarioComponent } from './form-usuario/form-usuario.component';
 @NgModule({
   declarations: [
     AppComponent,
-    FormUsuarioComponent
+    FormUsuarioComponent // Certifique-se de declarar o componente aqui
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
     MatInputModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
