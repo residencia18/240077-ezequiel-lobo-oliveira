719f4ec7021d3791e865c4e23793eb1876535764
EzekLobo <eloliveira.cic@uesc.br>
2024-03-09 02:05:07 -0300

    Adiciona rota de login e componentes de autenticação

--- PIs/PI14/petshop-app/src/app/app-routing.module.ts
+++ PIs/PI14/petshop-app/src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import { CadastroAtendimentoComponent } from './cadastro-atendimento/cadastro-at
 import { ListagemAtendimentosComponent } from './listagem-atendimentos/listagem-atendimentos.component';
 import { EdicaoAtendimentoComponent } from './edicao-atendimento/edicao-atendimento.component';
 import { DetalhamentoAtendimentoComponent } from './detalhamento-atendimento/detalhamento-atendimento.component';
+import { AuthComponent } from './auth/auth.component'; // Importe o componente de autenticação
 
 const routes: Routes = [
   { path: 'cadastro', component: CadastroAtendimentoComponent },
   { path: 'listagem', component: ListagemAtendimentosComponent },
   { path: 'edicao/:clienteCpf', component: EdicaoAtendimentoComponent },
   { path: 'detalhamento/:id', component: DetalhamentoAtendimentoComponent },
-  
+  { path: 'login', component: AuthComponent }, // Rota para o componente de autenticação (login)
 ];
 
 @NgModule({
--- PIs/PI14/petshop-app/src/app/auth/auth.service.ts
+++ PIs/PI14/petshop-app/src/app/auth/auth.service.ts
@@ -1,19 +1,32 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() {}
+  constructor(private afAuth: AngularFireAuth) {}
 
-  login(email: string, password: string): void {
-    // Lógica para fazer login
-    console.log('Login com:', email, password);
+  login(email: string, password: string): Promise<void> {
+    return this.afAuth.signInWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('Login bem-sucedido!');
+      })
+      .catch(error => {
+        console.error('Erro no login:', error);
+        throw error; 
+      });
   }
 
-  signUp(email: string, password: string): void {
-    // Lógica para cadastrar um novo usuário
-    console.log('Cadastro com:', email, password);
+  signUp(email: string, password: string): Promise<void> {
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('Cadastro bem-sucedido!');
+      })
+      .catch(error => {
+        console.error('Erro no cadastro:', error);
+        throw error; 
+      });
   }
 }
