e7933b609c578513b066858cb6d852e24f6a508c
EzekLobo <eloliveira.cic@uesc.br>
2024-03-10 23:40:04 -0300

    Adiciona roteamento e componentes para o dashboard

--- PIs/PI15/suinofarm/src/app/dashboard/dashboard.component.ts
+++ PIs/PI15/suinofarm/src/app/dashboard/dashboard.component.ts
@@ -5,4 +5,26 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {}
+export class DashboardComponent {
+  currentComponent: string = ''; // Variável para controlar o componente atualmente exibido
+
+  // Método para exibir o componente de cadastro de suínos
+  showCadastro() {
+    this.currentComponent = 'cadastro';
+  }
+
+  // Método para exibir o componente de listagem de suínos
+  showListagem() {
+    this.currentComponent = 'listagem';
+  }
+
+  // Método para exibir o componente de controle de peso
+  showControlePeso() {
+    this.currentComponent = 'controlePeso';
+  }
+
+  // Método para exibir o componente de edição de peso
+  showEdicaoPeso() {
+    this.currentComponent = 'edicaoPeso';
+  }
+}
