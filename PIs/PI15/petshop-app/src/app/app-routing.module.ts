0eca7d30016185b66288b9ad2df2c328b871411b
EzekLobo <eloliveira.cic@uesc.br>
2024-03-12 05:55:13 -0300

    Adiciona arquivos e faz alterações no código

--- PIs/PI15/petshop-app/src/app/app-routing.module.ts
+++ PIs/PI15/petshop-app/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroAtendimentoComponent } from './cadastro-atendimento/cadastro-atendimento.component';
+import { ListagemAtendimentosComponent } from './listagem-atendimentos/listagem-atendimentos.component';
+import { EdicaoAtendimentoComponent } from './edicao-atendimento/edicao-atendimento.component';
+import { DetalhamentoAtendimentoComponent } from './detalhamento-atendimento/detalhamento-atendimento.component';
+import { AuthComponent } from './auth/auth.component';
+import { AuthGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cadastro', component: CadastroAtendimentoComponent, canActivate: [AuthGuard] },
+  { path: 'listagem', component: ListagemAtendimentosComponent, canActivate: [AuthGuard] },
+  { path: 'edicao/:clienteCpf', component: EdicaoAtendimentoComponent, canActivate: [AuthGuard] },
+  { path: 'detalhamento/:id', component: DetalhamentoAtendimentoComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: AuthComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
