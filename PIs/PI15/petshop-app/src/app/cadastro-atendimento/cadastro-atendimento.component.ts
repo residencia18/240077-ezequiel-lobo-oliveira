38b5b89069d1289c2a2759f3c3c48b34a3fbc5bd
EzekLobo <eloliveira.cic@uesc.br>
2024-03-12 22:15:39 -0300

    Adicionando arquivos e diretórios iniciais para o projeto PetshopApp

---
+++ PIs/PI15/petshop-app/src/app/cadastro-atendimento/cadastro-atendimento.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+import { Atendimento } from '../Models/atendimento.model';
+import { AtendimentoService } from '../Services/atendimento.service';
+
+@Component({
+  selector: 'app-cadastro-atendimento',
+  templateUrl: './cadastro-atendimento.component.html',
+  styleUrls: ['./cadastro-atendimento.component.css']
+})
+export class CadastroAtendimentoComponent {
+  atendimentoForm: FormGroup;
+  atendimento: Atendimento = {
+    petName: '',
+    clienteName: '',
+    data: '',
+    observacoes: '',
+    clienteCpf: ''
+  };
+
+  constructor(private formBuilder: FormBuilder, private atendimentoService: AtendimentoService) {
+    this.atendimentoForm = this.formBuilder.group({
+      petName: ['', Validators.required],
+      clienteName: ['', Validators.required],
+      clienteCpf: ['', Validators.required],
+      data: ['', Validators.required],
+      observacoes: ['']
+    });
+  }
+
+  get petName() { return this.atendimentoForm.get('petName'); }
+  get clienteName() { return this.atendimentoForm.get('clienteName'); }
+  get clienteCpf() { return this.atendimentoForm.get('clienteCpf'); }
+  get data() { return this.atendimentoForm.get('data'); }
+
+  submitForm() {
+    if (!this.atendimentoForm) {
+      console.error('O formulário não está definido.');
+      return;
+    }
+
+    if (this.atendimentoForm.invalid) {
+      console.error('O formulário contém erros. Por favor, corrija-os antes de enviar.');
+      return;
+    }
+
+    this.atendimento = this.atendimentoForm.value;
+
+    this.atendimentoService.cadastrarAtendimento(this.atendimento)
+      .then(() => {
+        this.atendimentoForm.reset();
+      })
+      .catch(error => {
+        console.error('Ocorreu um erro durante o cadastro:', error);
+      });
+  }
+}
