c6e08c7aae4c321000affee483cfb5b6d4e53542
EzekLobo <eloliveira.cic@uesc.br>
2024-03-15 11:44:52 -0300

    Add project and program files for Exercicio3_Enum

---
+++ Praticas/PT-002/03/Exercicio3_Enum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+class Program
+{
+    enum Exercicio
+    {
+        Academia = 1,
+        Luta = 2,
+        Corrida = 3
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Opções de exercícios:");
+        Console.WriteLine($"{(int)Exercicio.Academia}: {Exercicio.Academia}");
+        Console.WriteLine($"{(int)Exercicio.Luta}: {Exercicio.Luta}");
+        Console.WriteLine($"{(int)Exercicio.Corrida}: {Exercicio.Corrida}");
+
+        Console.WriteLine("\nDigite o número correspondente ao exercício desejado (1, 2 ou 3):");
+        string input = Console.ReadLine();
+
+        try
+        {
+            int exercicioEscolhido = int.Parse(input);
+
+            if (Enum.IsDefined(typeof(Exercicio), exercicioEscolhido))
+            {
+                Exercicio opcao = (Exercicio)exercicioEscolhido;
+                Console.WriteLine($"Exercício escolhido: {opcao}");
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Por favor, digite um número válido (1, 2 ou 3).");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Formato inválido. Por favor, digite um número válido (1, 2 ou 3).");
+        }
+    }
+}
