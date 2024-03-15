e2dbcdec41bd331178f33f199e3e49e3f1dadc72
EzekLobo <eloliveira.cic@uesc.br>
2024-03-15 15:38:39 -0300

    Add Program.cs with code to create and execute a new instance of MeuServico

---
+++ Praticas/PT-002/04/Exercicio4_ServicoFabrica/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using Exercicio4_ServicoFabrica;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ServicoFabrica<MeuServico> fabrica = new ServicoFabrica<MeuServico>();
+        MeuServico servico = fabrica.NovaInstancia();
+        servico.Executar();
+    }
+}
