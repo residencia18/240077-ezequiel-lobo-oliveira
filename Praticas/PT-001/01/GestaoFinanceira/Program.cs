335896a0c498601b5048fab23e3cc3049072a96b
EzekLobo <eloliveira.cic@uesc.br>
2024-03-15 03:42:37 -0300

    Refactor program to include banking operations

--- Praticas/PT-001/01/GestaoFinanceira/Program.cs
+++ Praticas/PT-001/01/GestaoFinanceira/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using GestaoFinanceira;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ContaBancaria conta = new ContaBancaria(1000);
+
+        conta.Depositar(200);
+        conta.Sacar(500);
+        conta.Transferir(700, new ContaBancaria(0));
+    }
+}
