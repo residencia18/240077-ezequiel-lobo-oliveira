618de25bd15482df435697833364728ad55086e5
EzekLobo <eloliveira.cic@uesc.br>
2024-03-15 11:35:40 -0300

    Update project references and add exception handling example

---
+++ Praticas/PT-002/02/Exercicio2_ExceptionHandling/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            object o = null;
+            o.ToString();
+        }
+        catch (NullReferenceException ex)
+        {
+            Console.WriteLine("Erro: Tentativa de chamar o método ToString() em uma referência nula.");
+        }
+    }
+}
