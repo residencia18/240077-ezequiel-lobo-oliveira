ae09419f96645b233ff4c5452c18a1bd957c8072
EzekLobo <eloliveira.cic@uesc.br>
2024-03-18 02:25:15 -0300

    Add Enums.cs and ItemMercado.cs

---
+++ Praticas/PT-003/02/ListaDeCompras/Enums.cs
@@ -0,0 +1,8 @@
+
+public enum Tipo
+{
+    Comida,
+    Bebida,
+    Higiene,
+    Limpeza
+}
---
+++ Praticas/PT-003/02/ListaDeCompras/ItemMercado.cs
@@ -0,0 +1,7 @@
+
+public class ItemMercado
+{
+    public string Nome { get; set; }
+    public Tipo Tipo { get; set; }
+    public double Preco { get; set; }
+}
