2ec2a3a8c60ccdaf636eaf9fc56132e226e58a74
EzekLobo <eloliveira.cic@uesc.br>
2024-03-18 02:25:21 -0300

    Add delegate exercise solution

---
+++ Praticas/PT-003/01/DelegateExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando a lista inicial
+        List<double> lista = new List<double> { 3, 7, 2, 4, 6 };
+
+        // Chamando o método ConvertAll() para criar uma nova lista onde cada elemento é dividido por 2
+        List<double> novaLista = lista.ConvertAll(x => x / 2);
+
+        // Imprimindo os elementos da nova lista
+        novaLista.ForEach(x => Console.WriteLine(x));
+    }
+}
