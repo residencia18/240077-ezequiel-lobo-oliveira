8a37975b19bfeac7c80fddccc0ac0c7e5a2067b1
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-05-25 07:26:37 -0300

    Configuração RabbitMQ

--- src/Cepedi.Banco.Pessoa.Api/Controllers/EnderecoController.cs
+++ src/Cepedi.Banco.Pessoa.Api/Controllers/EnderecoController.cs
@@ -1,8 +1,13 @@
-﻿using Cepedi.Banco.Pessoa.Compartilhado.Requests;
+﻿﻿using Cepedi.Banco.Pessoa.Compartilhado.Requests;
+﻿using System.Text;
+using Cepedi.Banco.Pessoa.Compartilhado.Requests;
 using Cepedi.Banco.Pessoa.Compartilhado.Responses;
 using Cepedi.Banco.Pessoa.Dados;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Namespace;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace Cepedi.Banco.Pessoa.Api.Controllers;
 
@@ -12,11 +17,14 @@ public class EnderecoController : BaseController
 {
     private readonly ILogger<EnderecoController> _logger;
     private readonly ApplicationDbContext _context;
+    private readonly RabbitMQProducer _rabbitMQProducer;
 
     public EnderecoController(IMediator mediator, ILogger<EnderecoController> logger, ApplicationDbContext context) : base(mediator)
+    public EnderecoController(IMediator mediator, ILogger<EnderecoController> logger, ApplicationDbContext context, RabbitMQProducer rabbitMQProducer) : base(mediator)
     {
         _logger = logger;
         _context = context;
+        _rabbitMQProducer = rabbitMQProducer;
     }
 
     [HttpGet]
@@ -24,31 +32,26 @@ public class EnderecoController : BaseController
     {
         return await SendCommand(new ObterTodosEnderecosRequest());
     }
-
     [HttpGet("{id}")]
     public async Task<ActionResult<ObterEnderecoResponse>> ObterEndereco([FromRoute] int id)
     {
         return await SendCommand(new ObterEnderecoRequest() { EnderecoId = id });
     }
-
     [HttpGet("/cep/{cep}")]
     public async Task<ActionResult<ObterEnderecoPorCepResponse>> ObterEnderecoPorCep([FromRoute] string cep)
     {
         return await SendCommand(new ObterEnderecoPorCepRequest() { Cep = cep });
     }
-
     [HttpPost]
     public async Task<ActionResult<CadastrarEnderecoResponse>> CadastrarEndereco([FromBody] CadastrarEnderecoRequest request)
     {
         return await SendCommand(request);
     }
-
     [HttpPut("{id}")]
     public async Task<ActionResult<AtualizarEnderecoResponse>> AtualizarEndereco([FromBody] AtualizarEnderecoRequest request)
     {
         return await SendCommand(request);
     }
-
     [HttpDelete("{id}")]
     public async Task<ActionResult<ExcluirEnderecoResponse>> ExcluirEndereco([FromRoute] int id)
     {
@@ -56,4 +59,10 @@ public class EnderecoController : BaseController
         return await SendCommand(request);
     }
 
+    [HttpGet("rabbitMQ")]
+    public ActionResult SendMessage()
+    {
+        _rabbitMQProducer.Send("Hello World");
+        return Ok();
+    }
 }
