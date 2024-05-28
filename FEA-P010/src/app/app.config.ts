5d45953b693ca7b77c4fd88f71deac994210dbf0
Ezequiel Lobo <eloliveira.cic@uesc.br>
2024-05-28 10:08:34 -0300

    hosting

--- FEA-P010/src/app/app.config.ts
+++ FEA-P010/src/app/app.config.ts
@@ -1,9 +1,26 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideAnimations()]
 };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDREP7cKTlXDyUeXoE25zFtXyDUZla2CDM",
+  authDomain: "fir-86f08.firebaseapp.com",
+  databaseURL: "https://fir-86f08-default-rtdb.firebaseio.com",
+  projectId: "fir-86f08",
+  storageBucket: "fir-86f08.appspot.com",
+  messagingSenderId: "53684622438",
+  appId: "1:53684622438:web:5293ac92c79595d3e0c9a7",
+  measurementId: "G-6SJ4QL79KL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
